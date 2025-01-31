feaee0edd440a37ecb7e5ff91336a0d9f601449a
survaseatharv7 <atharvs7123@gmail.com>
2025-02-01 01:32:44 +0530

    Subscription Screen done

--- messify_owner/lib/pages/SubscriptionScreens/Subcription.dart
+++ messify_owner/lib/pages/SubscriptionScreens/Subcription.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
@@ -157,111 +161,166 @@ class Subcription extends StatefulWidget {
 }
 
 class _SubcriptionState extends State<Subcription> {
-  final ScrollController _scrollController = ScrollController();
-  int selectedDayIndex = DateTime.now().day - 1;
-  static int monthInt = DateTime.now().month;
-  int lastDate = LastDate(month: monthInt).lastDate;
+  // int selectedDayIndex = DateTime.now().day - 1;
+  dynamic selectedDate = DateTime.now();
+  DateTime now = DateTime.now();
+  // static int monthInt = DateTime.now().month;
+  // int lastDate = LastDate(month: monthInt).lastDate;
 
-  List userAttendanceList = [];
-  int buildContextCounter = 0;
+  // List userAttendanceList = [];
 
-  void userAttendanceListGetter(int day) async {
-    userAttendanceList.clear();
+  // void userAttendanceListGetter(DateTime date) async {
+  //   log("In userAttendance Method");
+  //   userAttendanceList.clear();
+  //   try {
+  //     QuerySnapshot response = await FirebaseFirestore.instance
+  //         .collection('Attendence')
+  //         .doc('${MainApp.messName}')
+  //         .collection(
+  //             '${DateTime.now().year}-${DateFormat("MM").format(DateTime.now())}-${DateFormat("dd").format(date)} ')
+  //         .get();
+  //     log("${DateTime.now().year}-${DateFormat("MM").format(DateTime.now())}-${DateFormat("dd").format(date)} ");
+  //     log("After response");
+  //     for (int i = 0; i < response.docs.length; i++) {
+  //       Map map = {};
+  //       map['username'] = response.docs[i].get('username');
+  //       map['name'] = response.docs[i].get('name');
+  //       map['token'] = response.docs[i].get('token').toString();
+  //       userAttendanceList.add(map);
+  //       log("User list = $userAttendanceList");
+  //       log("After list");
+  //       print(userAttendanceList);
+  //     }
+  //   } catch (e) {
+  //     log("${e}");
+  //   }
 
-    QuerySnapshot response = await FirebaseFirestore.instance
+  //   setState(() {});
+  // }
+
+  Stream<List<Map<String, dynamic>>> userAttendanceListStream(DateTime date) {
+    return FirebaseFirestore.instance
         .collection('Attendence')
         .doc('${MainApp.messName}')
-        .collection('${DateTime.now().year}-${monthInt}-${day} ')
-        .get();
-
-    for (int i = 0; i < response.docs.length; i++) {
-      Map map = {};
-      map['username'] = response.docs[i].get('username');
-      map['name'] = response.docs[i].get('name');
-      map['token'] = response.docs[i].get('token').toString();
-      userAttendanceList.add(map);
-      print(userAttendanceList);
-    }
-    setState(() {});
+        .collection(
+            '${DateFormat('yyyy').format(date)}-${DateFormat("MM").format(date)}-${DateFormat("dd").format(date)} ')
+        .snapshots()
+        .map((QuerySnapshot snapshot) {
+      log("Snapshot received");
+      List<Map<String, dynamic>> userList = [];
+      for (var doc in snapshot.docs) {
+        userList.add({
+          'username': doc['username'],
+          'name': doc['name'],
+          'token': doc['token'].toString(),
+        });
+      }
+      log("User list = $userList");
+      return userList;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    //   _scrollToSelectedDay(selectedDayIndex);
-    // });
+    // userAttendanceListGetter(DateTime.now());
   }
 
-  Widget cardBuilder(int index) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            gradient: LinearGradient(
-              colors: [Colors.orange.shade300, Colors.orange.shade600],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 6,
-                offset: Offset(0, 4),
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    color: Colors.blue,
-                    child: Image.asset("assets/avtar.jpeg", fit: BoxFit.cover),
-                  ),
-                ),
-                SizedBox(width: 16),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${userAttendanceList[index]['name']}",
-                        style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+  Widget cardBuilder() {
+    return StreamBuilder<List<Map<String, dynamic>>>(
+        stream: userAttendanceListStream(selectedDate),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator()); // Loading indicator
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text("No attendance data found."));
+          }
+
+          List<Map<String, dynamic>> userAttendanceList = snapshot.data!;
+
+          return ListView.builder(
+              itemCount: userAttendanceList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.orange.shade300,
+                            Colors.orange.shade600
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Container(
+                                width: 80,
+                                height: 80,
+                                color: Colors.blue,
+                                child: Image.asset("assets/avtar.jpeg",
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${userAttendanceList[index]['name']}",
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    "${userAttendanceList[index]['username']}",
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white70,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    "Token left: ${userAttendanceList[index]['token']}",
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white70,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(height: 4),
-                      Text(
-                        "${userAttendanceList[index]['username']}",
-                        style: GoogleFonts.poppins(
-                          color: Colors.white70,
-                          fontSize: 14,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        "Token left: ${userAttendanceList[index]['token']}",
-                        style: GoogleFonts.poppins(
-                          color: Colors.white70,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
+                    ));
+              });
+        });
   }
 
   // void _scrollToSelectedDay(int index) {
@@ -278,18 +337,19 @@ class _SubcriptionState extends State<Subcription> {
   //   );
   // }
 
+  void updateDate(DateTime newDate) {
+    setState(() {
+      selectedDate =
+          newDate; // Update the state and trigger StreamBuilder refresh
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-     dynamic selectedDate = DateTime.now();
-  DateTime now = DateTime.now();
+    String formattedDateSelected =
+        DateFormat('dd/MM/yyyy').format(selectedDate);
+    String formattedDateCurrent = DateFormat('dd/MM/yyyy').format(now);
 
-    String formattedDateSelected = DateFormat('dd/MM/yyyy').format(selectedDate);
-   String formattedDateCurrent = DateFormat('dd/MM/yyyy').format(now);
-    buildContextCounter++;
-    if (buildContextCounter == 1) {
-      userAttendanceListGetter(selectedDayIndex + 1);
-      print(userAttendanceList);
-    }
     return Scaffold(
       body: Column(
         children: [
@@ -300,36 +360,38 @@ class _SubcriptionState extends State<Subcription> {
           //     mainAxisAlignment: MainAxisAlignment.spaceAround,
           //     children: [
           //       Text(" Todays Date: $formattedDateCurrent",style: GoogleFonts.quicksand(textStyle: TextStyle(fontSize: 15,fontWeight: FontWeight.w700)),),
-               
+
           //       Text(" Selected Date: $formattedDateSelected",style: GoogleFonts.quicksand(textStyle: TextStyle(fontSize: 15,fontWeight: FontWeight.w700)),),
           //     ],
           //   ),
-            const SizedBox(height: 10,),
-            HorizontalWeekCalendar(
-              minDate: DateTime(2000, 1, 1),
-              maxDate: DateTime(2030, 1, 31),
-              initialDate: selectedDate,
-              onDateChange: (date) {
-                setState(() {
-                  selectedDate = date;
-                });
-              },
-              //showTopNavbar: false,
-              monthFormat: "MMMM yyyy",
-              showNavigationButtons: false,
-              weekStartFrom: WeekStartFrom.Sunday,
-              borderRadius: BorderRadius.circular(7),
-              activeBackgroundColor: Colors.orangeAccent,
-              activeTextColor: Colors.white,
-              inactiveBackgroundColor: Colors.orangeAccent.withOpacity(.3),
-              inactiveTextColor: Colors.white,
-              disabledTextColor: Colors.grey,
-              disabledBackgroundColor: Colors.grey.withOpacity(.3),
-              activeNavigatorColor: Colors.deepOrange,
-              inactiveNavigatorColor: Colors.grey,
-              monthColor: Colors.deepOrange,
-            ),
-            
+          const SizedBox(
+            height: 10,
+          ),
+          HorizontalWeekCalendar(
+            minDate: DateTime(2000, 1, 1),
+            maxDate: DateTime(2030, 1, 31),
+            initialDate: selectedDate,
+            onDateChange: (date) {
+              // int selectedDate = int.parse(DateFormat('dd').format(date));
+              updateDate(date);
+              setState(() {});
+            },
+            //showTopNavbar: false,
+            monthFormat: "MMMM yyyy",
+            showNavigationButtons: false,
+            weekStartFrom: WeekStartFrom.Sunday,
+            borderRadius: BorderRadius.circular(7),
+            activeBackgroundColor: Colors.orangeAccent,
+            activeTextColor: Colors.white,
+            inactiveBackgroundColor: Colors.orangeAccent.withOpacity(.3),
+            inactiveTextColor: Colors.white,
+            disabledTextColor: Colors.grey,
+            disabledBackgroundColor: Colors.grey.withOpacity(.3),
+            activeNavigatorColor: Colors.deepOrange,
+            inactiveNavigatorColor: Colors.grey,
+            monthColor: Colors.deepOrange,
+          ),
+
           // Container(
           //   height: 70,
           //   width: MediaQuery.of(context).size.width, // Adjust height as needed
@@ -357,13 +419,7 @@ class _SubcriptionState extends State<Subcription> {
           SizedBox(
             height: 50,
           ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: userAttendanceList.length,
-                itemBuilder: (context, index) {
-                  return cardBuilder(index);
-                }),
-          ),
+          Expanded(child: cardBuilder()),
           Padding(
             padding: EdgeInsets.all(MainApp.widthCal(20)),
             child: Container(
@@ -389,7 +445,7 @@ class _SubcriptionState extends State<Subcription> {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(8)),
                     elevation: 5,
-                    minimumSize: Size.fromHeight(50),
+                    // minimumSize: Size.fromHeight(50),
                   ),
                   onPressed: () {
                     Navigator.push(
@@ -406,7 +462,7 @@ class _SubcriptionState extends State<Subcription> {
               ),
             ),
           ),
-      ],
+        ],
       ),
     );
   }
